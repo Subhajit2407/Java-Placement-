0b30e94ad0c143cf87cf83f5bd3278df4dbc4f25
Subhajit2407 <subhajitbtechinternship24@gmail.com>
2026-03-25 15:12:04 +0530

    Add files via upload

---
+++ Main13.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input size
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        // Input elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        // Count even and odd numbers
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        // Output
+        System.out.println("Even numbers count: " + evenCount);
+        System.out.println("Odd numbers count: " + oddCount);
+    }
+}
